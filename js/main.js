fd3ca795e313668c3bfd087658d0471f67861e4d
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2024-04-12 09:07:01 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,286 @@
+const translation={
+    english:{
+        special_title:"Our Transport Services",
+        special_paragraph:"Discover our premium transport services, encompassing klia/klia2/Subang/ Johor Bahru/Pulau Pinang airport transfer., hotel transfers, holiday packages and transportation for exploring the vibrant destinations. We prioritize your comfort, safety, and convenience, offering expert chauffeurs and well-maintained vehicles. Whether you're arriving, departing, or sightseeing, we're here to make your journey seamless and memorable.",
+        Provide:"We Provide:",
+        KLIA:"KLIA & KLIA 2 Airport Transport",
+        Johor:"Johor Bahru Airport Transport",
+        Hotel :"Hotel Transport",
+        Singapore :'Singapore & Penang Transport',
+        startRm:"Start from RM99/ pax",
+        Experience:"Experience the Best Journey with the Most Affordable Price",
+        Embark:"Embark on an unforgettable journey that combines quality and affordability with our chauffeured tour. Our curated experiences offer you the best of travel without compromising your budget. Choose from a variety of travel packages and enjoy the ultimate adventure at the most competitive prices",
+        Airport :"Airport Klia from/to Kuala Lumpur City.",
+        Sedan :"Sedan Car RM100",
+        SUV :"SUV Car RM130",
+        MPV :"MPV RM160",
+        XL:"XL MPV RM250",
+        Seater:"10 Seater Comfort Van RM300",
+        Midnight:"* Midnight trip subject to surcharge",
+        Cuti:'Go "Cuti-Cuti Malaysia" with affordable budget, start from RM 99 / pax',
+        fes4Text:" Embark on a budget-friendly Malaysian adventure with our tour packages, exploring all 10 states. From the lively streets of Kuala Lumpur to the tranquil beaches of Langkawi, our itineraries promise a diverse experience. Discover the rich heritage of Melaka and the vibrant culture of Penang. We offer affordable, quality travel, ensuring you can explore Malaysia's beauty without breaking the bank. Let us guide you through this extraordinary journey, creating lasting memories in every state.",
+        KL_City:"KL City day tour 4 person RM500 only",
+        Kuala:"Kuala Lumpur TOUR",
+        View_More:"View More",
+        Batu:"Batu Cave and Genting Highlands day tour 4 person RM600 only",
+        Selangor:"Selangor TOUR",
+        KL_Penang:"KL to Penang Transfer 4 person RM600 only",
+        Penang:"Penang TOUR",
+        Melaka:"Melaka day tour 4 person RM600 only",
+        Melaka_TOUR:"Melaka TOUR",
+        Kedah:"Kedah TOUR",
+        Pahang_TOUR:"Pahang TOUR",
+        Terengganu:"Terengganu TOUR",
+        Terengganu:"Terengganu TOUR",
+        Perak_TOUR:"Perak TOUR",
+        Negeri:"Negeri Sembilan TOUR",
+        Johor_TOUR:"Johor TOUR",
+        POPULAR:"POPULAR",
+        POPULARBold:"TOURS",
+        POPULARText:"Our packages are designed to suit both solo travellers and groups. Whether you're seeking a solo escape or planning a memorable trip with friends or family, we've got you covered. Our travel package will transport you to an exotic destination that's been handpicked for its breathtaking beauty and cultural richness. Whether it's a tropical paradise, a historic city, or an off-the-beaten-path gem, you're in for a treat.",
+        SALE:"SALE",
+        Family :"Family Leisure",
+        Kids:"Family Kids",
+        Time:"Family Time",
+        School:"School Holiday",
+        Public_Holiday:"Public Holiday",
+        Ticket:"Ticket-Free",
+        Budget:"Budget Travelers",
+        Economical:"Economical",
+        Retirees:"Retirees",
+        Students:"Students",
+        Education:"Education",
+        educatStudents:"Students",
+        School_Trip:"School Trip",
+        Learning_Trip:"Learning Trip",
+        Knowledgeable:"Knowledgeable",
+        Foodie:"Foodie",
+        Lovers:"Food Lovers",
+        Influencers:"Influencers",
+        Bloggers:"Food Bloggers",
+         Culture:"Food Culture",
+         Destinations:"Must Eat Destinations",
+         Natural:"Natural",
+         Enthusiasts:"Nature Enthusiasts",
+         Wildlife :"Wildlife Encounters",
+         Eco_Tourism:"Eco-Tourism",
+        Beauty:"Natural Beauty",
+        Social:"Social Media Hotspot",
+        socialInfluencers:"Influencers",
+        Instagrammable:"Instagrammable",
+        socialBloggers:"Bloggers",
+        Vloggers:"Vloggers",
+        TikTokers:"TikTokers",
+        Historical:"Historical Bulding",
+        FamilyKids:"Family Kids",
+        artEnthusiasts:"Art & Design Enthusiasts",
+        Buffs:"History Buffs",
+        CultureLovers:"Art & Culture Lovers",
+        Romantic:"Romantic Dating",
+        Couples:"Couples",
+        Honeymooners:"Honeymooners",
+        Celebrating:"Celebrating Special Occasions",
+        Ourpackage :"Our package includes",
+        ourDriver:"Driver",
+        hassle_free:"Enjoy a hassle-free and safe journey with our professional driver",
+        Fare:"Fare",
+        fareText:"Flexibility to explore your destination at your own pace by package include convenient car fares",
+        Toll:"Toll",
+        Tolltext:" Enjoy a smooth and uninterrupted journey without toll fares",
+        Parking:"Parking",
+        Parkingtext:"Say goodbye to parking hassles and unexpected charges",
+        Custompickup:"Custom pickup",
+        Custompickuptext:"Personalized pick-up options, making your trip uniquely yours",
+        Mineralwater:"Mineral water",
+        Mineralwatertext:" We provide you complimentary mineral water in all our transports, ensuring you remain comfortable as you explore",
+        why:"why",
+        whybold:"Choose Us",
+        Expertisebold:"Expertise",
+        Expertisetext:"Our team brings years of experience and in-depth knowledge of diverse destinations to the table.",
+        Variety:"Variety",
+        VarietyText:"We provide multiple choice of itineraries.",
+        Personalization:"Personalization",
+        Personalizationtext:" We tailor each trip to the needs of each group and accept additions and deletions from the itinerary.",
+        Convenient:"Convenient",
+        Convenienttext:"We plan the itinerary and prepare transportation for you.",
+        CustomerCentric:"Customer-Centric",
+        CustomerCentrictext:"Your satisfaction and peace of mind are our top priorities. We are here to support you throughout your journey.",
+        ExploreText:"Explore Our Customer's Adventures Through Stunning Tour Snaps",
+        Immerse:"Immerse yourself in the joy of travel with glimpses from our customers' past tours. Explore Malaysia through their eyes in these stunning snapshots—each a testament to the happiness and unforgettable moments we create. Let these images inspire your next adventure with us. ",
+        AboutUs:"About",
+        Aboutbold:"Us",
+        Abouttext:"At PRO TEAM HOLIDAY SDN BHD, we are passionate about transforming ordinary journeys into extraordinary adventures. Our mission is to provide you with seamless travel experiences that can save our customers time but can create lasting memories.We take the stress out of planning and let you focus on the joy of exploring the world. Our dedicated team of travel experts works tirelessly to curate seamless travel packages that cater to your unique preferences, interests, and desires.",
+        KualaTilte:" Kuala Lumpur",
+    },
+    chineese:{
+        special_title:"我们的运输服务",
+        special_paragraph:"探索我们的优质交通服务，包括 klia/klia2/梳邦/新山/槟城机场接送、酒店接送、度假套餐以及探索充满活力的目的地的交通。我们优先考虑您的舒适、安全和便利，提供专业的司机和维护良好的车辆。无论您抵达、离开还是观光，我们都会让您的旅程顺利而难忘",
+        Provide:"我们提供：",
+        KLIA:"KLIA 和 KLIA 2 机场交通",
+        Johor:"新山机场交通",
+        Hotel :"酒店交通",
+        Singapore:"新加坡和槟城交通",
+        startRm:" 从...开始 RM99 / 人数",
+        Experience:"体验 最佳旅程 最多 可接受的价格",
+        Embark:"通过我们的专职司机之旅，踏上一次难忘的旅程，将品质与价格相结合。我们精心策划的体验为您提供最佳的旅行体验，同时又不影响您的预算。多种选择 旅行套餐，以最具竞争力的价格享受终极冒险",
+        Airport :"吉隆坡国际机场往返吉隆坡市。",
+        Sedan :"轿车 RM100",
+        SUV :"SUV车 RM130",
+        MPV :"多用途车 RM160",
+        XL:"XL MPV RM250",
+        Seater:"10 座舒适面包车 RM300",
+        Midnight:"* 午夜行程需支付附加费",
+        Cuti:"去“Cuti-Cuti Malaysia”吧  实惠的预算，每人 RM 99 起",
+        fes4Text:"通过我们的旅游套餐，踏上经济实惠的马来西亚冒险之旅，探索所有 10 个州。从吉隆坡热闹的街道到兰卡威宁静的海滩，我们的行程保证为您带来多样化的体验。探索马六甲丰富的遗产和槟城充满活力的文化。我们提供价格实惠、优质的旅行，确保您无需花太多钱即可探索马来西亚的美景。让我们引导您完成这段非凡的旅程，在每个州创造持久的回忆。",
+        KL_City:"吉隆坡市区一日游4人只需RM500",
+        Kuala:"吉隆坡之旅",
+        View_More:"查看更多",
+        Batu:"黑风洞 & 云顶高原一日游 4 人只需 RM600",
+        Selangor:"雪兰莪旅游",
+        KL_Penang:"吉隆坡至槟城接送 4 人只需 RM600",
+        Penang:"槟城旅游",
+        Melaka:"马六甲一日游4人只需RM600",
+        Melaka_TOUR:"马六甲旅游",
+        Kedah :"吉打之旅",
+        Terengganu:"登嘉楼州旅游",
+        Pahang_TOUR:"彭亨旅游",
+        Terengganu:"登嘉楼州旅游",
+        Perak_TOUR:"霹雳游",
+        Negeri:"森美兰旅游",
+        Johor_TOUR:"柔佛旅游",
+        POPULAR:"受欢迎的",
+        POPULARBold:"旅游",
+        POPULARText:"我们的套餐专为适合单独旅行者和团体旅行者而设计。无论您是寻求独自逃离，还是计划与朋友或家人一起度过难忘的旅行，我们都能满足您的需求。我们的旅行套餐将带您前往充满异国情调的目的地，该目的地因其令人惊叹的美景和丰富的文化而精心挑选。无论是热带天堂、历史名城还是人迹罕至的瑰宝，您都将得到享受。",
+        SALE:"销售",
+        Family:"家庭休闲",
+        Kids:"家庭儿童",
+        Time:"家人在一起的时间",
+        School:"学校假期",
+        Public_Holiday:"公众假期",
+        Ticket:"免票",
+        Budget:"预算旅行者",
+        Economical:"经济",
+        Retirees:"退休人员",
+        Students:"学生",
+        Education:"教育",
+        educatStudents:"学生",
+        School_Trip:"校园旅行",
+        Learning_Trip:"学习之旅",
+        Knowledgeable:"知识渊博",
+        Foodie:"美食家",
+        Lovers:"美食爱好者",
+        Influencers:"影响者",
+        Bloggers:"美食博主",
+         Culture:"饮食文化",
+         Destinations:"必吃目的地",
+         Natural:"自然的",
+         Enthusiasts:"自然爱好者",
+         Wildlife :"邂逅野生动物",
+         Eco_Tourism:"生态旅游",
+        Beauty:"自然美",
+        Social:"社交媒体热点",
+        socialInfluencers:"影响者",
+        Instagrammable:"适合 Instagram 的",
+        socialBloggers:"博主",
+        Vloggers:"视频博主",
+        TikTokers:"抖音用户",
+        Historical:"历史建筑",
+        FamilyKids:"家庭儿童",
+        artEnthusiasts:"艺术与艺术设计爱好者",
+        Buffs:"历史爱好者",
+        CultureLovers:"艺术与艺术文化爱好者",
+        Romantic:"浪漫约会",
+        Couples:"情侣",
+        Honeymooners:"度蜜月的人",
+        Celebrating:"庆祝特殊场合",
+        Ourpackage :"我们的套餐包括",
+        ourDriver:"司机",
+        hassle_free:"与我们的专业司机一起享受无忧无虑、安全的旅程",
+        Fare:"票价",
+        fareText:"灵活地按照自己的节奏探索目的地，包括便捷的汽车票价",
+        Toll:"收费",
+        Tolltext:" 享受顺畅不间断的旅程，无需支付过路费",
+        Parking:"停車處",
+        Parkingtext:"告别停车麻烦和意外收费",
+        Custompickup:"定制皮卡",
+        Custompickuptext:"个性化的接送选项，让您的旅程独一无二",
+        Mineralwater:"矿泉水",
+        Mineralwatertext:"我们在所有交通工具中为您提供免费矿泉水，确保您在探索时保持舒适",
+        why:"为什么",
+        whybold:"选择我们",
+        Expertisebold:"专业知识",
+        Expertisetext:"我们的团队拥有多年的经验和对不同目的地的深入了解。",
+        Variety:"种类",
+        VarietyText:"我们提供多种行程选择。",
+        Personalization:"个性化",
+        Personalizationtext:" 我们根据每个团体的需求定制每次旅行，并接受行程的添加和删除。",
+        Convenient:"方便的",
+        Convenienttext:"我们为您规划行程并准备交通。",
+        CustomerCentric:"以顾客为中心",
+        CustomerCentrictext:"您的满意和安心是我们的首要任务。我们将在您的整个旅程中为您提供支持。",
+        ExploreText:"通过令人惊叹的旅游快照探索客户的冒险经历",
+        Immerse:"回顾我们客户过去的旅行，让自己沉浸在旅行的乐趣中。在这些令人惊叹的快照中通过他们的眼睛探索马来西亚 - 每一张都证明了我们创造的幸福和难忘的时刻。让这些图像激发您与我们的下一次冒险。",
+        AboutUs:"关于",
+        Aboutbold:"我们",
+        Abouttext:"在 PRO TEAM HOLIDAY SDN BHD，我们热衷于将平凡的旅程转变为非凡的冒险。我们的使命是为您提供无缝的旅行体验，不仅可以节省客户的时间，还可以创造持久的回忆。我们消除了规划的压力，让您专注于探索世界的乐趣。我们的专业旅行专家团队孜孜不倦地努力策划无缝旅行套餐，以满足您独特的喜好、兴趣和愿望。",
+        KualaTilte:"吉隆坡",
+    }
+}
+
+// const selectLanguage=document.querySelector("select")
+
+// selectLanguage.addEventListener("change",(e)=>{
+//     setLanguage(e.target.value)
+// })
+const setLanguage=(lang)=>{
+   
+    const elements = document.querySelectorAll('[data-i18n]')
+    // console.log(elements)
+    elements.forEach((ele)=>{
+      
+        const translationKey=ele.getAttribute('data-i18n')
+        // console.log(translationKey)
+        // console.log(translation[lang][translationKey])
+        ele.textContent=translation[lang][translationKey]
+    })
+
+}
+localStorage.setItem("lang","chineese")
+// window.addEventListener(("load"),()=>{
+//     setLanguage("chineese")
+// })
+
+// logic chang
+
+const select = document.querySelector(".select");
+const options_list = document.querySelector(".options-list");
+const options = document.querySelectorAll(".option");
+
+//show & hide options list
+select.addEventListener("click", (e) => {
+   
+  options_list.classList.toggle("active");
+  select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+});
+
+//select option
+options.forEach((option) => {
+    option.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
+        localStorage.setItem("lang",e.target.textContent)
+const getLang=localStorage.getItem("lang")
+console.log(getLang)
+        setLanguage(getLang)
+  
+});
+  option.addEventListener("click", () => {
+   
+    options.forEach((option) => { option.classList.remove('selected') });
+    select.querySelector("span").innerHTML = option.innerHTML;
+    option.classList.add("selected");
+    options_list.classList.toggle("active");
+    select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
+  });
+});
+
+console.log("1111111")
