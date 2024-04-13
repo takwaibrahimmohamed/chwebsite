c29356f7c5b3bfb2751302004b7eb4d4dccd9f0f
takwaibrahimmohamed <takwaibrahimmohamed2002@gmail.com>
2024-04-13 19:41:56 +0200

    card

--- js/main.js
+++ js/main.js
@@ -111,7 +111,45 @@ const translation={
         Aboutbold:"Us",
         Abouttext:"At PRO TEAM HOLIDAY SDN BHD, we are passionate about transforming ordinary journeys into extraordinary adventures. Our mission is to provide you with seamless travel experiences that can save our customers time but can create lasting memories.We take the stress out of planning and let you focus on the joy of exploring the world. Our dedicated team of travel experts works tirelessly to curate seamless travel packages that cater to your unique preferences, interests, and desires.",
         KualaTilte:" Kuala Lumpur",
-    },
+        VIP_Services:"VIP Services",
+        PartTime:"Part-Time:",
+        Unarmed:"Unarmed Bodyguard",
+        rateUnarmed:"Rate: 2,700 CNY for 8 hours",
+        OvertimeUnarmed:"Overtime: 150 CNY per hour after 8 hours",
+        Armed:"Armed Bodyguard",
+        ArmedRate:"Rate: 5,000 CNY for 8 hours",
+        ArmedOvertime:"Overtime: 300 CNY per hour after 8 hours",
+        Driver:"Driver",
+        DriverRate:"Rate: 1,800 CNY for 8 hours",
+        DriverOvertime:"Overtime: 105 CNY per hour after 8 hours",
+        FullTime:"Full-Time",
+        FullTimeSalary:'Monthly Salary: 18,000 CNY for 24 days of work (8 hours/day)',
+        FullTimeTerms:"Terms: One-year contract with the possibility to discuss overtime and public holidays; requires a 1+1 deposit",
+        Armedsalary:"Monthly Salary: 27,000 CNY for 24 days of work (8 hours/day)",
+        Armedterm:"Terms: One-year contract with the possibility to discuss overtime and public holidays; requires a 1+1 deposit",
+        fulltimedriver:"Monthly Salary: 15,000 CNY for 26 days of work (8 hours/day)",
+        fulltimeterm:"Terms: One-year contract with the possibility to discuss overtime and public holidays; requires a 1+1 deposit",
+        Armedimg:"Armed Bodyguard",
+        Unarmedimg:"Unarmed Bodyguard",
+        KrisDayanti:"Client: Kris Dayanti, is an Indonesian singer and actress",
+        Hajah:"Client Dato' Sri Hajah Siti Nurhaliza",
+   Hajahmore:"Tarudin SSAP,DIMP, JSM, SAP, PMP, AAP is a Malaysian singer, songwriter, actress and businesswoman with more than 300 local and international awards",
+   Zulkifli:"Client: Dato Sri Dr Zulkifli Mohamad Al-Bakri",
+   Zulkiflimore:"is a Malaysian Islamic scholar who served as  Minister in the Prime Minister's Department in charge of Religious Affairs in the Perikatan Nasional (PN) administration under former Prime Minister Muhyiddin Yassin from March 2020 to August 2021, Senator from March 2020 to March 2023 and 7th Mufti of the Federal Territories from June 2014 to his political appointments in March 2020.",
+   Abderrazak:"Client: Abderrazak Makri",
+   Abderrazakmore:"is the leader president of Algeria’s largest legal Islamist party",
+   Serge:"Client: Sir Serge",
+   Santos:"Client: Don Santos",
+   Wira:"Client: Datuk Wira Hajah Mas  Ermieyati binti",
+   Wiramore:"Samsudin is a Malaysian politician who has served as Chairperson of the Public Accounts Committee (PAC) since April 2023",
+   Mahathir:"Client: Tun Dr. Mahathir bin Mohamad is",
+   Mahathirmore:"a Malaysian politician, author, and physician who served as the fourth and seventh prime minister of Malaysia Tan Sri Lee Kim Yew is the founder of the Country Heights Group of Companies, Tan Sri Lee is the man behind the growth and success of the Country Heights Group where its major business divisions include property development, property investment, hospitality, education and health.",
+    Senator:"Client: YB Senator Saraswathy Kandasami ",
+    Senatormore:"is a Malaysian politician and lawyer who has served as the Deputy Minister of National Unity in the Unity Government administration",
+  Mohd:"Client: Datuk Seri Mohd Redzuan bin Md",
+Mohdmore:"Yusof is a Malaysian politician who served as Minister in the Prime Minister's Department for Special Functions in the Perikatan Nasional (PN) administration under former Prime Minister Muhyiddin Yassin from March 2020 to August 2021",
+readmore:"read more",
+  },
     chineese:{
         special_title:"我们的运输服务",
         special_paragraph:"探索我们的优质交通服务，包括 klia/klia2/梳邦/新山/槟城机场接送、酒店接送、度假套餐以及探索充满活力的目的地的交通。我们优先考虑您的舒适、安全和便利，提供专业的司机和维护良好的车辆。无论您抵达、离开还是观光，我们都会让您的旅程顺利而难忘",
@@ -120,26 +158,26 @@ const translation={
         Johor:"新山机场交通",
         Hotel :"酒店交通",
         Singapore:"新加坡和槟城交通",
-        startRm:" 从...开始 RM99 / 人数",
+        startRm:" 从...开始 371.25 CNY/pax",
         Experience:"体验 最佳旅程 最多 可接受的价格",
         Embark:"通过我们的专职司机之旅，踏上一次难忘的旅程，将品质与价格相结合。我们精心策划的体验为您提供最佳的旅行体验，同时又不影响您的预算。多种选择 旅行套餐，以最具竞争力的价格享受终极冒险",
         Airport :"吉隆坡国际机场往返吉隆坡市。",
-        Sedan :"轿车 RM100",
-        SUV :"SUV车 RM130",
-        MPV :"多用途车 RM160",
-        XL:"XL MPV RM250",
-        Seater:"10 座舒适面包车 RM300",
+        Sedan :"轿车 375 CNY/pax",
+        SUV :"SUV车 487.5 CNY/pax",
+        MPV :"多用途车 600 CNY/pax",
+        XL:"XL MPV 937.5 CNY/pax",
+        Seater:"10 座舒适面包车 1125 CNY/pax",
         Midnight:"* 午夜行程需支付附加费",
-        Cuti:"去“Cuti-Cuti Malaysia”吧  实惠的预算，每人 RM 99 起",
+        Cuti:"去“Cuti-Cuti Malaysia”吧  实惠的预算，每人 371.25 CNY/pax 起",
         fes4Text:"通过我们的旅游套餐，踏上经济实惠的马来西亚冒险之旅，探索所有 10 个州。从吉隆坡热闹的街道到兰卡威宁静的海滩，我们的行程保证为您带来多样化的体验。探索马六甲丰富的遗产和槟城充满活力的文化。我们提供价格实惠、优质的旅行，确保您无需花太多钱即可探索马来西亚的美景。让我们引导您完成这段非凡的旅程，在每个州创造持久的回忆。",
-        KL_City:"吉隆坡市区一日游4人只需RM500",
+        KL_City:"吉隆坡市区一日游4人只需1875CNY/pax",
         Kuala:"吉隆坡之旅",
         View_More:"查看更多",
-        Batu:"黑风洞 & 云顶高原一日游 4 人只需 RM600",
+        Batu:"黑风洞 & 云顶高原一日游 4 人只需 2250CNY/pax",
         Selangor:"雪兰莪旅游",
-        KL_Penang:"吉隆坡至槟城接送 4 人只需 RM600",
+        KL_Penang:"吉隆坡至槟城接送 4 人只需 2250CNY/pax",
         Penang:"槟城旅游",
-        Melaka:"马六甲一日游4人只需RM600",
+        Melaka:"马六甲一日游4人只需2250CNY/pax",
         Melaka_TOUR:"马六甲旅游",
         Kedah :"吉打之旅",
         Terengganu:"登嘉楼州旅游",
@@ -224,6 +262,45 @@ const translation={
         Aboutbold:"我们",
         Abouttext:"在 PRO TEAM HOLIDAY SDN BHD，我们热衷于将平凡的旅程转变为非凡的冒险。我们的使命是为您提供无缝的旅行体验，不仅可以节省客户的时间，还可以创造持久的回忆。我们消除了规划的压力，让您专注于探索世界的乐趣。我们的专业旅行专家团队孜孜不倦地努力策划无缝旅行套餐，以满足您独特的喜好、兴趣和愿望。",
         KualaTilte:"吉隆坡",
+
+        VIP_Services:"贵宾服务",
+        PartTime:"兼职：",
+        Unarmed:"手无寸铁的保镖",
+        rateUnarmed:"价格：2,700元/8小时",
+        OvertimeUnarmed:"加班：8小时后每小时150元",
+        Armed:"武装保镖",
+        ArmedRate:"费用：5,000元/8小时",
+        ArmedOvertime:"加班：8小时后每小时300元",
+        Driver:"司机",
+        DriverRate:"费用：1,800元/8小时",
+        DriverOvertime:"加班：8小时后每小时105元",
+        FullTime:"全职",
+        FullTimeSalary:'月薪：18,000元，工作24天（8小时/天）',
+        FullTimeTerms:"条款：一年合同，可以讨论加班和公共假期；需要1+1押金",
+        Armedsalary:"月薪： 27,000 元，工作 24 天（8 小时/天）",
+        Armedterm:"条款：一年合同，可以讨论加班和公共假期；需要1+1押金",
+        fulltimedriver:"月薪：15,000元/工作26天（8小时/天）",
+        fulltimeterm:"条款：一年合同，可以讨论加班和公共假期；需要1+1押金",
+        Armedimg:"武装保镖",
+        Unarmedimg:"手无寸铁的保镖",
+        KrisDayanti:"客户：Kris Dayanti，印度尼西亚歌手兼演员",
+        Hajah:"客户拿督斯里哈贾西蒂努哈丽扎",
+        Hajahmore:"Tarudin SSAP、DIMP、JSM、SAP、PMP、AAP 是一位马来西亚歌手、词曲作者、演员和女商人，荣获 300 多个本地和国际奖项",
+     Zulkifli:"客户：拿督斯里祖基菲里·穆罕默德·巴克里博士",
+     Zulkiflimore:"马来西亚伊斯兰学者，曾于2020年3月至2021年8月在前首相慕尤丁·亚辛领导的国盟政府中担任总理府负责宗教事务的部长，于2020年3月至2023年3月担任参议员和第七任穆夫提从 2014 年 6 月担任联邦直辖区议员到 2020 年 3 月接受政治任命。",
+ Abderrazak:"客户：阿卜杜拉扎克·马克里",
+ Abderrazakmore:"是阿尔及利亚最大的合法伊斯兰政党的领导人",
+ Serge:"客户：塞尔日爵士",
+ Santos:"客户：唐·桑托斯",
+ Wira:"客户：Datuk Wira Hajah Mas Ermieyati binti",
+ Wiramore:"Samsudin是马来西亚政治家，自2023年4月起担任公共账目委员会（PAC）主席",
+ Mahathir:"客户：敦马哈迪博士",
+ Mahathirmore:"马来西亚政治家、作家和医生，曾担任马来西亚第四任和第七任首相 丹斯里李金友是 Country Heights 集团公司的创始人，丹斯里李是 Country Heights 成长和成功背后的人物集团主要业务包括房地产开发、房地产投资、酒店、教育和健康。",
+ Senator:"客户：YB 参议员 Saraswathy Kandasami",
+ Senatormore:"马来西亚政治家和律师，曾担任团结政府行政部门民族团结副部长",
+ Mohd:"客户：Datuk Seri Mohd Redzuan bin Md",
+Mohdmore:"尤索夫是一名马来西亚政治家，于2020年3月至2021年8月期间担任前总理穆希丁·亚辛领导下的国民联盟（PN）政府总理特别职能部部长",
+readmore:"阅读更多",
     }
 }
 
@@ -297,5 +374,46 @@ options.forEach((option) => {
     select.querySelector(".fa-angle-down").classList.toggle("fa-angle-up");
   });
 });
+  
+// var dots = document.querySelectorAll(".dots");
+// var moreText = document.querySelectorAll(".more");
+var btnText = document.querySelectorAll(".myBtn");
 
-console.log("1111111")
+btnText.forEach((btn)=>{
+
+  btn.addEventListener("click", (e)=>{
+    const dots =btn.parentElement.children[1]
+    const moreText =btn.parentElement.children[2]
+    console.log(btn.parentElement.children.classList)
+    console.log(btn.parentElement.children[1])
+    console.log(btn.parentElement.children[2])
+   
+    console.log(btn)
+    if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    
+    if(localStorage.getItem("lang") =="english"){
+  
+      btn.innerHTML = "Read more"; 
+    }
+    else{
+      btn.innerHTML = "阅读更多"; 
+    }
+    
+   
+    moreText.style.display = "none";
+    } else {
+    dots.style.display = "none";
+    if(localStorage.getItem("lang") =="english"){
+      btn.innerHTML = "Read less"; 
+    }
+    else{
+      btn.innerHTML = "少读书"; 
+    }
+    
+    moreText.style.display = "inline";
+    }
+  
+   
+  })
+})
